7d50fc1a6e702e5debf94d8d703834cd85498191
priyanshiisingh <priyanshianamika1815@gmail.com>
2022-12-21 20:27:56 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
+// import * as dotenv from "dotenv";
+// dotenv.config();
 
-function App() {
+const YOUR_SERVICE_ID = "service_flzwp8n";
+const YOUR_TEMPLATE_ID = "template_l5frmub";
+const YOUR_PUBLIC_KEY = "dEriyd1zzF2EssiKC";
+const App = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        YOUR_SERVICE_ID,
+        YOUR_TEMPLATE_ID,
+        form.current,
+        YOUR_PUBLIC_KEY
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          alert(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+          alert("email not sent");
+        }
+      );
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form ref={form} onSubmit={sendEmail}>
+      <label>Name</label>
+      <input type="text" name="user_name" />
+      <label>Email</label>
+      <input type="email" name="user_email" />
+      <label>Message</label>
+      <textarea name="message" />
+      <input type="submit" value="Send" />
+    </form>
   );
-}
+};
 
 export default App;
